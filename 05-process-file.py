21f9ccc636e6ca0848bfbaa470a35f1f95b1804d
Mod Wongprasert <mod.amonnat@gmail.com>
2026-01-21 00:57:01 -0800

    Update and rename process_file.py to 05-process-file.py

---
+++ 05-process-file.py
@@ -0,0 +1,24 @@
+def process_all_files(folder_path):
+    data_list = []
+    
+    # Loop throughall files
+    for f in Path(folder_path).iterdir():
+        
+        # Check if it's a file
+        if f.is_file():
+            try:
+                # Read the file content
+                content = f.read_text(encoding='utf-8')
+
+                # Extract transactions and add to main list
+                data_list.extend(extract_transactions(content, source_file=f.name))                            
+            except Exception as error:
+                #If file can't be read, print error with the file name and continue
+                print(f"Error {f.name}: {error}")
+
+    df = pd.DataFrame(data_list)
+    print(f"Done! Extracted {len(df)} rows from {len(list(Path(folder_path).glob('*')))} files.")                                                               
+    return df  
+
+# Run
+df_final = process_all_files(r"D:\BCIT\Winter 2026\BI2\CS1\Dataset")
